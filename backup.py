fd79120ba26b234dc1a366b10b93438a4fc74aed
jd13313 <31113043+jd13313@users.noreply.github.com>
2017-08-17 17:27:05 -0400

    Create backup.py

---
+++ backup.py
@@ -0,0 +1,61 @@
+import os
+import datetime
+import tarfile
+
+backup_list = [
+    {
+        'path': 'C:\\Users\Jeremy\Documents',
+        'name': 'Local Documents'
+    },
+    {
+        'path': 'C:\\Users\Jeremy\Desktop',
+        'name': 'Desktop'
+    },
+    {
+        'path': 'C:\\Users\Jeremy\Pictures',
+        'name': 'Local Pictures'
+    },
+    {
+        'path': 'C:\World of Warcraft\Screenshots',
+        'name': 'WoW Screenshots'
+    },
+    {
+        'path': 'C:\World of Warcraft\Interface',
+        'name': 'WoW Plugins'
+    },
+    {
+        'path': 'D:\Projects',
+        'name': 'Projects'
+    },
+    {
+        'path': 'Z:\Docs',
+        'name': 'Server Documents'
+    },
+    {
+        'path': 'Z:\Photos',
+        'name': 'Server Photos'
+    },
+    {
+        'path': 'Z:\Sites',
+        'name': 'Server Sites'
+    }
+]
+
+
+current_date = datetime.datetime.now()
+destination_dir = 'Z:\Backups\\' + current_date.strftime('%Y/%m/%d')
+
+if not os.path.exists(destination_dir):
+    os.makedirs(destination_dir)
+
+for dir in backup_list:
+    print('Backing up ' + dir['path'] + '...')
+
+    try:
+        tar = tarfile.open(destination_dir + '/' + dir['name'] + '.tar.bz2', 'w:bz2', compresslevel=9)
+        tar.add(dir['path'], arcname=dir['name'])
+        tar.close()
+    except IOError:
+        print('Window sucks, skipping this non-existent file...')
+    finally:
+        print(dir['path'] + ' backed up successfully!')
